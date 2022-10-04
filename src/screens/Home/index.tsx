62da8d08a74d0e545089f35c1ebe847f290232c6
amanda-santos <amandasf0200@gmail.com>
2022-10-04 20:06:17 -0300

    feat: 💄 add input

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,19 @@
-import { Text, View } from "react-native";
+import { ReactElement } from "react";
+import { Text, TextInput, View } from "react-native";
 
 import { styles } from "./styles";
 
-export const Home = () => {
+export const Home = (): ReactElement => {
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Event name</Text>
       <Text style={styles.eventDate}>Friday, Oct 7th</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Guest name"
+        placeholderTextColor="#6b6b6b"
+      />
     </View>
   );
 };
