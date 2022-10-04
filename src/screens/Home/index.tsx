a4da795435f6dd98a25737b14be84185c26e1877
amanda-santos <amandasf0200@gmail.com>
2022-10-04 20:15:56 -0300

    feat: 💄 style form

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,19 +1,29 @@
 import { ReactElement } from "react";
-import { Text, TextInput, View } from "react-native";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 
 import { styles } from "./styles";
 
 export const Home = (): ReactElement => {
+  const handleAddParticipant = () => {
+    console.log("Add participant");
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Event name</Text>
       <Text style={styles.eventDate}>Friday, Oct 7th</Text>
 
-      <TextInput
-        style={styles.input}
-        placeholder="Guest name"
-        placeholderTextColor="#6b6b6b"
-      />
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Participant name"
+          placeholderTextColor="#6b6b6b"
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleAddParticipant}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -4,7 +4,8 @@ export const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#131016",
-    padding: 24,
+    paddingHorizontal: 24,
+    paddingTop: 72,
   },
   eventName: {
     color: "#FDFCFE",
@@ -16,11 +17,31 @@ export const styles = StyleSheet.create({
     fontSize: 16,
   },
   input: {
+    flex: 1,
     backgroundColor: "#1F1E25",
     height: 56,
     borderRadius: 5,
     color: "#FDFCFE",
     padding: 16,
     fontSize: 16,
+    marginRight: 16,
+  },
+  buttonText: {
+    color: "#FDFCFE",
+    fontSize: 24,
+  },
+  button: {
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: "#31CF67",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  form: {
+    width: "100%",
+    flexDirection: "row",
+    marginTop: 32,
+    marginBottom: 40,
   },
 });
